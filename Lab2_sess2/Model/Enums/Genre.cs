b0cc140a4e45741154d61fa66644d225147616c7
Nooruz Suleimanov <suleimanovnooruz@gmail.com>
2026-02-15 22:28:24 +0700

    Add project files.

---
+++ Lab2_sess2/Model/Enums/Genre.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_sess2.Model.Enums
+{
+
+    
+
+    internal class Genre
+    {
+        public enum Gerge
+        {
+            Comedy,
+            Drama,
+            Thriller,
+            Action,
+            Horror,
+            Blockbuster
+        }
+    }
+}
